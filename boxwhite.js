34a9ad39b27d82ca5fa75ef0562e6bde93b1078d
Tanmayprajapati8 <73639638+Tanmayprajapati8@users.noreply.github.com>
2021-01-04 22:55:45 +0530

    Add files via upload

---
+++ boxwhite.js
@@ -0,0 +1,23 @@
+class Boxwhite {
+    constructor(x,y,width,height) {
+      var options = {
+        isStatic: false,
+        restitution:0.3,
+        friction:1.0,
+        density:1.0
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("white");
+      strokeWeight(4);
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
